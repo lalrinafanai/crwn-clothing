eef6ec55c783ca577dd6536afe4129738cd97e4f
“lalrinafanai” <lalrina3113fanai@gmail.com>
2020-06-15 22:17:56 +0530

    created project with create react app and added file for homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,46 @@
+/* ---------- IMPORT ---------- */
+import React from 'react';
+
+import './homepage.styles.scss';
+
+/* ---------- FUNCTIONAL COMPONENT ---------- */
+/* No life-cycle methods nor storing state is not needed */
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>JACKETS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>SNEAKERS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>WOMENS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>MANS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default HomePage;
+
